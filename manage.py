6cb89118183b2d54808475a0b0aa69a95e4a5318
JOnas <docdocjonas@gmail.com>
2023-03-22 20:33:49 +0800

    changes

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks. Test"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bcdh_pos.settings')
     try:
         from django.core.management import execute_from_command_line
