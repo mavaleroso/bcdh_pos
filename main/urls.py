58c6b2d948ccc0989f9f4beed6598762ce48f1ca
Marwen <marwenvaleroso@gmail.com>
2023-03-28 23:03:47 +0800

    login

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
