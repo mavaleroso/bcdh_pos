2ca61253bab0208596746978bbee912dc31cdba2
Reymark <reymarkvaldehueza@gmail.com>
2023-04-19 21:28:34 +0800

    feature/18: Display List of Brand, Sub generic, Company and Users using Datatable

--- bcdh_pos/settings.py
+++ bcdh_pos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@1d=!6svu4gy(xf!*1vj@qgb68ti7u#)ye!uq(cjna1t@9ievi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- main/models.py
+++ main/models.py
@@ -116,6 +116,7 @@ class Company(models.Model):
     code = models.CharField(max_length=128, blank=True, null=True)
     address = models.CharField(max_length=128, blank=True, null=True)
     remarks = models.CharField(max_length=300, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=datetime.now,blank=True, null=True)
     updated_at = models.DateTimeField(default=datetime.now,blank=True, null=True)
     class Meta:
@@ -142,6 +143,7 @@ class SubGeneric(models.Model):
 
 class Brand(models.Model):
     name = models.CharField(max_length=128, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=datetime.now,blank=True, null=True)
     updated_at = models.DateTimeField(default=datetime.now,blank=True, null=True)
     class Meta:
