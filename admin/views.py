4974625e35bb28b8234196e8d93fc30f3160a6f5
JOnas <docdocjonas@gmail.com>
2023-04-24 21:22:10 +0800

    feature/20 Create Datatables for Brand, Sub-generic & Units

--- admin/views.py
+++ admin/views.py
@@ -45,6 +45,12 @@ def subgeneric(request):
 	}
     return render(request, 'admin/sub_generic.html', context)
 
+def units(request):
+    context = {
+		'units' : SubGeneric.objects.filter().order_by('name'),
+	}
+    return render(request, 'admin/units.html', context)
+
 def user(request):
     context = {
 		'sub_generic' : SubGeneric.objects.filter().order_by('name'),
@@ -82,7 +88,101 @@ def updategeneric(request):
         if check_generic:
             Generic.objects.filter(id=generic_id).update(name=generic_name, is_active=status)
             return JsonResponse({'data': 'success'})
+
+@csrf_exempt
+def addbrand(request):
+    if request.method == 'POST':
+        check_brand = False
+        brand_name = request.POST.get('brandname')
+        if Brand.objects.filter(name=brand_name):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_brand = True        
+        if check_brand:
+            add = Brand(
+                name= brand_name)
+            add.save()
+            return JsonResponse({'data': 'success'})
         
 
+@csrf_exempt
+def updatebrand(request):
+    if request.method == 'POST':
+        brand_id = request.POST.get('brand_id')
+        brand_name = request.POST.get('brandname')
+        status = request.POST.get('is_active')
 
+        check_brand = False
+        brand_name = request.POST.get('brandname')
+        if Brand.objects.filter(name=brand_name).exclude(id=brand_id):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_brand = True        
+        if check_brand:
+            Brand.objects.filter(id=brand_id).update(name=brand_name, is_active=status)
+            return JsonResponse({'data': 'success'})
+
+@csrf_exempt
+def addsubgeneric(request):
+    if request.method == 'POST':
+        check_brand = False
+        subgeneric_name = request.POST.get('subgenericname')
+        if SubGeneric.objects.filter(name=subgeneric_name):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_subgeneric = True        
+        if check_subgeneric:
+            add = SubGeneric(
+                name= subgeneric_name)
+            add.save()
+            return JsonResponse({'data': 'success'})
+
+@csrf_exempt
+def updatesubgeneric(request):
+    if request.method == 'POST':
+        subgeneric_id = request.POST.get('subgeneric_id')
+        subgeneric_name = request.POST.get('subgenericname')
+        status = request.POST.get('is_active')
+
+        check_subgeneric = False
+        subgeneric_name = request.POST.get('subgenericname')
+        if SubGeneric.objects.filter(name=subgeneric_name).exclude(id=subgeneric_id):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_subgeneric = True        
+        if check_subgeneric:
+            SubGeneric.objects.filter(id=subgeneric_id).update(name=subgeneric_name, is_active=status)
+            return JsonResponse({'data': 'success'})
+
+@csrf_exempt
+def addunits(request):
+    if request.method == 'POST':
+        check_units = False
+        units_name = request.POST.get('unitsname')
+        if Units.objects.filter(name=units_name):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_units = True        
+        if check_units:
+            add = Units(
+                name= units_name)
+            add.save()
+            return JsonResponse({'data': 'success'})
+
+@csrf_exempt
+def updateunits(request):
+    if request.method == 'POST':
+        units_id = request.POST.get('units_id')
+        units_name = request.POST.get('unitsname')
+        status = request.POST.get('is_active')
+
+        check_units = False
+        units_name = request.POST.get('unitsname')
+        if Units.objects.filter(name=units_name).exclude(id=units_id):
+            return JsonResponse({'data': 'error'})
+        else:
+            check_units = True        
+        if check_units:
+            Units.objects.filter(id=units_id).update(name=units_name, is_active=status)
+            return JsonResponse({'data': 'success'})
 
