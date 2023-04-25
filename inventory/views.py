870e8d15f0990bca87be0b9757dbec7a49ea2bca
Marwen Valeroso <marwenvaleroso@gmail.com>
2023-04-25 22:07:13 +0800

    initial commit

--- inventory/views.py
+++ inventory/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from main.models import (Item, ItemDetails, SystemConfiguration, ItemType, Company, Generic, SubGeneric, Brand, Unit)
+from main.models import (Item, SystemConfiguration, ItemType, Company, Generic, SubGeneric, Brand, Unit)
 import datetime
 
 
@@ -51,11 +51,14 @@ def store_items(request):
         total_quantity_pcs = request.POST.get('TotalQuantityPcs')
         retail_price_unit = request.POST.get('RetailPriceUnit') if request.POST.get('RetailPriceUnit') else 0
         retail_price_pcs = request.POST.get('RetailPricePcs')
+        barcode = request.POST.get('Barcode')
+        expiration_date = request.POST.get('ExpirationDate')
+        delivery_date = request.POST.get('DeliveredDate')
         user_id = request.session.get('user_id', 0)
         code = generate_code()
        
-        item_add = Item(code=code, type_id=item_type, company_id=company, generic_id=generic, sub_generic_id=sub_generic,
-                        description=description, brand_id=brand, unit_price=unit_price, unit_id=item_unit, unit_quantity=item_quantity, retail_price_unit=retail_price_unit, retail_price=retail_price_pcs, user_id=user_id)
+        item_add = Item(code=code, barcode=barcode, type_id=item_type, company_id=company, generic_id=generic, sub_generic_id=sub_generic,
+                        description=description, brand_id=brand, unit_price=unit_price, unit_id=item_unit, unit_quantity=item_quantity, pcs_quantity=total_quantity_pcs, retail_price_unit=retail_price_unit, retail_price=retail_price_pcs, delivered_date=delivery_date, expiration_date=expiration_date, user_id=user_id)
 
         item_add.save()
 
@@ -63,14 +66,6 @@ def store_items(request):
             system_config = SystemConfiguration.objects.first()
             system_config.inventory_code = code
             system_config.save()
-
-        counter = item_quantity if int(item_quantity) > 0 else total_quantity_pcs
-        for i in range(int(counter)):
-            barcode = request.POST.get('barcode['+str(i)+']')
-            expiry_date = request.POST.get('expiryDate['+str(i)+']')
-            item_details_add = ItemDetails(
-                item_id=item_add.id, quantity=item_quantity_pcs, barcode=barcode, expiration_date=expiry_date)
-            item_details_add.save()
 
         return JsonResponse({'data': 'success'})
 
