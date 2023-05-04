9f49ce707dcec25d05b7bf4fa240e459b8a0f25f
Reymark <reymarkvaldehueza@gmail.com>
2023-05-04 15:25:53 +0800

    feature/25:incomplete_bigboyz

--- sales/views.py
+++ sales/views.py
@@ -24,66 +24,19 @@ def salestransaction(request):
 	}
     return render(request, 'sales/transaction.html', context)
 
+    
 @csrf_exempt
 def patientdetails(request):
     patient_id = request.POST.get('patient')
-    # clients = Clients.objects.filter(id=patient_id).select_related()
-    # qs_json = serializers.serialize('json', clients, fields=['first_name', 'middle_name', 'client_type__name'])
+    qs_list = list(
+         (Clients.objects
+             .filter(id=patient_id)
+             .select_related('client_type')
+             .values('first_name','middle_name','last_name', 'client_type__name')
+         )
+    )
+    return JsonResponse({'data': qs_list})
 
-    # clients = (Clients.objects
-    # .filter(id=patient_id )
-    # .select_related('client_type')
-    # #use F to evaluate supplier_type__name and add to new field
-    # .annotate(client_type_name = F('client_type__name'))
-    # )
-    # qs_json = serializers.serialize(
-    #     'json', 
-    #     clients, 
-    #     #use our new field for serialization
-    #     fields=['first_name', 'middle_name', 'client_type__name']
-    # )
-
-
-    clients = Clients.objects.filter(client_type_id=patient_id)
-    qs_json = serializers.serialize('json', clients, fields=['first_name', 'middle_name','last_name',  'client_type__name'])
-
-    print("watmeme")
-    print(qs_json) 
-    return JsonResponse({'data': qs_json})
-
-
-
-
-
-
-
-
-# def patientdetails(request):
-    
-#     patient_id = request.POST.get('patient')
-#     # patient_details = Clients.objects.filter(id=patient_id).select_related()
-    
-#     # qs_json = serializers.serialize('json', patient_details)
-
-
-#     context = {
-# 		'company' : Clients.objects.filter(id=patient_id).select_related("clientType__name")
-    
-# 	}
-
-#     # qs_json = serializers.serialize('json', context)
-
-#     # tabledata = Clients.objects.filter(id=patient_id).annotate(creator_name=F('creator__name')).values()
-        
-
-#     # return JsonResponse(qs_json)
-
-    
-#     # patient_id = request.POST.get('patient')
-#     # patient_details = Clients.objects.filter(id=patient_id).select_related()
-    
-#     qs_json = serializers.serialize('json', context)
-#     return JsonResponse({'data': qs_json})
 
         
 
