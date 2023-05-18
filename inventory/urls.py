5a40a0b07d93d500ccecfb85ecc653af74971884
Marwen Valeroso <marwenvaleroso@gmail.com>
2023-05-19 06:22:43 +0800

    initial commit

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('in', views.inventory_in, name='inventory-in'),
     path('store_items', views.store_items, name='store-items'),
     path('list', views.inventory_list, name='inventory-list'),
-    path('list/edit', views.inventory_list_edit, name='inventory-list-edit'),
+    path('list/edit/', views.inventory_list_edit,
+         name='inventory-list-edit'),
     path('load', views.inventory_load, name='inventory-load'),
     path('received_item_load', views.received_item_load, name='received-item-load'),
 ]
