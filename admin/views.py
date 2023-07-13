445e55c0f7c68c66fe54b5aaa84bb83725bc0f01
Reymark N. Valdehueza <rnvaldehueza@ENTDSWD.LOCAL>
2023-07-14 00:51:25 +0800

    feature/56:Server side user with change password

--- admin/views.py
+++ admin/views.py
@@ -16,6 +16,9 @@ from django.core.serializers import serialize
 import datetime
 from django.contrib.auth.hashers import make_password
 import math
+from django.db.models import Max
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
 
 
@@ -159,53 +162,75 @@ def updateclients(request):
 
 @csrf_exempt
 def user_add(request):
-    user_ = request.POST.get('User')
-    code_ = request.POST.get('Code')
-    address_ = request.POST.get('Address')
-    remarks_ = request.POST.get('Remarks')
-    user_id = request.session.get('user_id', 0)
-    user_add = AuthUser(name=user_, code=code_, address=address_,remarks=remarks_)
     try:
+        user_name = request.POST.get('Username')
+        firstname = request.POST.get('Firstname')
+        middlename = request.POST.get('Middlename')
+        lastname = request.POST.get('Lastname')
+        password = request.POST.get('Password')
+        email = request.POST.get('Email')
+        role_id = request.POST.get('Roles')
+        birth_date = request.POST.get('Birthdate')
+        sex = request.POST.get('Sex')
+        address = request.POST.get('Address')
+        position = request.POST.get('Position')
+        password = make_password(password)
+        user_id = request.session.get('user_id', 0)
+        user_add = AuthUser(password = password,is_superuser=role_id,username=user_name,first_name=firstname,last_name=lastname,email=email,date_joined=timezone.now())
         user_add.save()
+        max_id = AuthUser.objects.aggregate(max_id=Max('id'))['max_id']
+        user_details_add = UserDetails(middle_name=middlename, birthdate=birth_date, sex=sex,address=address,position=position,role_id = role_id,user_id = max_id, added_by_user_id = user_id)
+        user_details_add.save()
+
         return JsonResponse({'data': 'success'})
     except Exception as e:
         return JsonResponse({'data': 'error'})
-        
+    
+#End User function ---------------->
+
 @csrf_exempt
 def user_update(request):
-    id = request.POST.get('ItemID')
-    user_ = request.POST.get('User')
-    code_ = request.POST.get('Code')
-    address_ = request.POST.get('Address')
-    remarks_ = request.POST.get('Remarks')
-    status = request.POST.get('Status')
-
-    if AuthUser.objects.filter(name=user_).exclude(id=id):
-        return JsonResponse({'data': 'error', 'message': 'Duplicate User'})
-    else:
-        AuthUser.objects.filter(id=id).update(name=user_, code=code_, address=address_,remarks=remarks_,is_active=status)
-        return JsonResponse({'data': 'success'})
+    try:
+        id = request.POST.get('ItemID')
+        user_name = request.POST.get('Username')
+        firstname = request.POST.get('Firstname')
+        middlename = request.POST.get('Middlename')
+        lastname = request.POST.get('Lastname')
+        password = request.POST.get('Password')
+        email = request.POST.get('Email')
+        role_id = request.POST.get('Roles')
+        birth_date = request.POST.get('Birthdate')
+        sex = request.POST.get('Sex')
+        address = request.POST.get('Address')
+        position = request.POST.get('Position')
+        password = make_password(password)
+        user_id = request.session.get('user_id', 0)
+        status = request.POST.get('Status')
         
+        AuthUser.objects.filter(id=id).update(password = password,is_superuser=role_id,username=user_name,first_name=firstname,last_name=lastname,email=email,is_active = status)
+        UserDetails.objects.filter(user_id=id).update(middle_name=middlename, birthdate=birth_date, sex=sex,address=address,position=position,role_id = role_id)
+        return JsonResponse({'data': 'success'})
+    except Exception as e:
+        return JsonResponse({'data': 'error'})
 
 def user_edit(request):
     id = request.GET.get('id')
     items = AuthUser.objects.get(pk=id)
+    userdetail = UserDetails.objects.get(user_id=id)
     data = serialize("json", [items])
+    data = json.loads(data)
+    userdetail_data = serialize("json", [userdetail])
+    userdetail_data = json.loads(userdetail_data)
+    data[0]['fields'].update(userdetail_data[0]['fields'])
+    data = json.dumps(data)
     return HttpResponse(data, content_type="application/json")
 
-def user_load(request):
-    print("Testttttaaa")
-    user_data = AuthUser.objects.select_related().order_by('-date_joined').reverse()
+def user_load(request):    
+    user_data = AuthUser.objects.select_related('userdetails').order_by('-date_joined')
+    id_list = [item.id for item in user_data]
     total = user_data.count()
-    
-    id_list = [user.id for user in user_data]
-
-   
-    
-    user_details = UserDetails.objects.filter(user_id__in=id_list).select_related()
-    
-    
-    
+    data = []
+    user_details = UserDetails.objects.filter(user_id__in=id_list)
 
     _start = request.GET.get('start')
     _length = request.GET.get('length')
@@ -218,63 +243,29 @@ def user_load(request):
         user_data = user_data[start:start + length]
         
         
-    data = []
-
-    for item in user_details:
-        user_details_item = {
-            'user_details_id': item.id,
-            'middle_name': item.middle_name,
-            'birthdate': item.birthdate,
-            'sex': item.sex,
-            'address': item.address,
-            'position': item.position,
-        }
-        data.append(user_details_item)
-
     for item in user_data:
-        user_data_item = {
-            'id': item.id,
-            'username': item.username,
-            'first_name': item.first_name,
-            'last_name': item.last_name,
-            'email': item.email,
-            'is_active': item.is_active,
-        }
-        data.append(user_data_item)
-    
-
-    # data = []
-    
-    # for item in user_details:
-    #     item = {
-            
-    #         'user_details_id': item.id,
-    #         'middle_name': item.middle_name,
-    #         'birthdate': item.birthdate,
-    #         'sex': item.sex,
-    #         'address': item.address,
-    #         'position': item.position,
-    #     }
-    #     data.append(item)
-
-    # for item in user_data:
-    #     item = {
-            
-    #         'id': item.id,
-    #         'username': item.username,
-    #         'first_name': item.first_name,
-    #         'last_name': item.last_name,
-    #         'email': item.email,
-    #         'is_active': item.is_active,
-
-
-    #     }
-    #     data.append(item)
-    
-    
-    
+        user_details_item = next((detail for detail in user_details if detail.user_id == item.id), None)
+        if user_details_item:
+            role_name = user_details_item.role.role_name if user_details_item.role else None
+            user_data_item = {
+                'user_details_id': user_details_item.id,
+                'middle_name': user_details_item.middle_name,
+                'birthdate': user_details_item.birthdate,
+                'sex': user_details_item.sex,
+                'address': user_details_item.address,
+                'position': user_details_item.position,
+                'role_name': role_name,
+                'id': item.id,
+                'username': item.username,
+                'first_name': item.first_name,
+                'last_name': item.last_name,
+                'email': item.email,
+                'is_active': item.is_active,
+            }
+            data.append(user_data_item)
 
     response = {
+        'data_user': data,
         'data': data,
         'page': page,
         'per_page': per_page,
