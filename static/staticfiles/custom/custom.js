0bdea918becaf1b0bfa01f761d2728cf28b37255
Marwen <marwenvaleroso@gmail.com>
2023-08-02 06:13:11 +0800

    finish

--- static/staticfiles/custom/custom.js
+++ static/staticfiles/custom/custom.js
@@ -93,3 +93,11 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+function convertDateTime(date_var) {
+    let date = new Date(date_var);
+    let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+    let month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+    let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+    return `${day}-${month}-${year}`;
+}
